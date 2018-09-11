1dda6f33635e59c355dad21e8aec84ba9e921cb5
PinguMac <>
2018-09-11 14:55:30 +0800

    Create new demo - User defined Literals

---
+++ UserDefinedLiterals/UserDefinedLiterals/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UserDefinedLiterals
+//
+//  Created by PinguMac on 2018/9/11.
+//  Copyright © 2018年 PinguMac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
